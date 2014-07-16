379442d56897149c3c2decaf16e5031a3f5ac427
jdediego <jrdo@gft.com>
2014-07-16 11:56:13 +0200

    Fixed problem number to word

--- src/test/java/com/me/quizs/WordifiedNumberImplTest.java
+++ src/test/java/com/me/quizs/WordifiedNumberImplTest.java
@@ -29,7 +29,6 @@ public class WordifiedNumberImplTest {
     public static void tearDownClass() {
     }
 
-    
 
     @Before
     public void setUp() {
@@ -41,14 +40,14 @@ public class WordifiedNumberImplTest {
 
     @Test
     public void wordifiedWordReferenceTest() throws Exception {
-        WordifiedNumberImpl wordifiedNumber = new WordifiedNumberImpl();
+        WordifiedNumber wordifiedNumber = new WordifiedNumberImpl();
         assertEquals("one", wordifiedNumber.toWords(1));
         assertEquals("twenty one", wordifiedNumber.toWords(21));
         assertEquals("one hundred and five", wordifiedNumber.toWords(105));
-        assertEquals("one thousand and five", wordifiedNumber.toWords(1005));
-//        assertEquals("one hundred and ten thousand one hundred and five", wordifiedNumber.toWords(110105)); 
-//        assertEquals("one million and ninety nine", wordifiedNumber.toWords(1000099));
-//        assertEquals("one million one hundred", wordifiedNumber.toWords(1000100));
-//        assertEquals("fifty six million nine hundred and forty five thousand seven hundred and eighty one", wordifiedNumber.toWords(56945781));
+        assertEquals("one thousand and five", wordifiedNumber.toWords(1005)); 
+        assertEquals("one hundred and ten thousand one hundred and five", wordifiedNumber.toWords(110105)); 
+        assertEquals("one million and ninety nine", wordifiedNumber.toWords(1000099));
+        assertEquals("one million one hundred", wordifiedNumber.toWords(1000100));
+        assertEquals("fifty six million nine hundred and forty five thousand seven hundred and eighty one", wordifiedNumber.toWords(56945781));
     }
 }
