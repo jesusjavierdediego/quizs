4368b032fb1af976bd89dc606a3937aec33551dd
administrator <administrator@Node>
2014-07-25 16:56:31 +0200

    Updated quizs

--- src/main/java/com/me/quizs/FrogRiverOne.java
+++ src/main/java/com/me/quizs/FrogRiverOne.java
@@ -1,26 +1,65 @@
 package com.me.quizs;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+A small frog wants to get to the other side of a river. The frog is currently located at position 0, and wants to get to position X. Leaves fall from a tree onto the surface of the river.
+You are given a non-empty zero-indexed array A consisting of N integers representing the falling leaves. A[K] represents the position where one leaf falls at time K, measured in minutes.
+The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only when leaves appear at every position across the river from 1 to X.
+For example, you are given integer X = 5 and array A such that:
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+In minute 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the river.
+Write a function:
+class Solution { public int solution(int X, int[] A); }
+that, given a non-empty zero-indexed array A consisting of N integers and integer X, returns the earliest time when the frog can jump to the other side of the river.
+If the frog is never able to jump to the other side of the river, the function should return −1.
+For example, given X = 5 and array A such that:
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+the function should return 6, as explained above. Assume that:
+N and X are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..X].
 
-
-
-/**
- *
- * @author administrator
- */
+*/
 public class FrogRiverOne {
     public static void main(String[] args) {
-        int[] start = {4,1,2};
-        System.out.println(solution(start));
+        int steps = 5;
+        int[] leaves = {1, 3, 1, 4, 2, 3, 5, 4};
+        System.out.println(solution(steps, leaves));
     }
     
-    static int solution(int[] A) {
+    static int solution(int steps, int[] leaves) {
 
-           return 0;
+        
+        boolean[] bitmap = new boolean[steps+1];
+        
+        int i = 0;
+        for(int step:leaves){
+            if(!bitmap[leaves[i]]){
+                bitmap[leaves[i]] = true;
+                steps--;
+            }
+            if(steps == 0) {
+               return i; 
+            }else{
+                i++;
+            }
+                
+        }
+
+        return -1;
+
     }
     
 }
---
+++ src/main/java/com/me/quizs/MissingInteger.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.me.quizs;
+
+/*
+ Write a function:
+ class Solution { public int solution(int[] A); }
+ that, given a non-empty zero-indexed array A of N integers, 
+ returns the minimal positive integer that does not occur in A.
+ For example, given:
+ A[0] = 1    
+ A[1] = 3    
+ A[2] = 6
+ A[3] = 4    
+ A[4] = 1    
+ A[5] = 2
+ the function should return 5.
+
+ */
+public class MissingInteger {
+
+    public static void main(String[] args) {
+        int[] start = {1, 3, 6, 4, 1, 2};
+        System.out.println(solution(start));
+    }
+
+    public static int solution(int[] A) {
+        int result = 0;
+        for(int i=0;i<A.length;i++){
+            int factor = A[i];
+            boolean check = false;
+            for(int j=0;j<A.length;j++){
+                int next = A[j];
+                if(factor+1 == next){
+                    check = true;
+                }
+            }
+            if(!check && factor+1 < getMaxValue(A)){
+                result = factor+1;
+                break;
+            }
+        }
+       return result;
+
+    }
+    
+    private static int getMaxValue(int[] array){  
+        int maxValue = array[0];  
+        for(int i=1;i < array.length;i++){  
+        if(array[i] > maxValue){  
+        maxValue = array[i];  
+
+           }  
+       }  
+       return maxValue;  
+  }  
+
+}
